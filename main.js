0ce19fda061247f63b8ee8e458d054738294d300
luizeduardoufg <luizeduardo.ufg@gmail.com>
2020-09-01 13:33:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const { window } = new JSDOM("")
 const $ = require("jquery")(window)
 const path = require('path')
 const fetchArt = require('./utils/fetchArt.js')
-const e = require('express')
+
 
 let gens = [
     'axe', 'bossa-nova', 'forro', 'funk', 'funk-carioca',
@@ -25,6 +25,7 @@ const map = (lang) => {
     switch (lang) {
         case 1: return 'pt'
         case 2: return 'en'
+        case 3: return 'es'
         default: return lang
     }
 }
@@ -36,15 +37,18 @@ const requestMusic = (artist, song) => {
         + "&mus=" + song
         + "&apikey={key}"
     ).then(function (data) {
-        if (data.mus[0].lang === 1)
-            return [data.mus[0].text, data.mus[0].lang]
-        console.log(`The music ${song} is not brazilian.`)
-        return null
+        return [data.mus[0].text, data.mus[0].lang]
     })
 }
 
 
 const writeFile = (gen, art, songName, mus) => {
+    let songPath = path.join(__dirname, 'Genders', gen, art, `${songName.replace(slash, '')}.json`)
+    let artFolderPath = path.join(__dirname, 'Genders', gen, art)
+    if (mus[1] != 1){
+        console.log(`The music ${songName} is not brazilian.`)
+        songPath = path.join(__dirname, 'Genders', gen, art, `${songName.replace(slash, '')}.txt`)
+    }
     let music = {
         artist: art,
         song: {
@@ -54,22 +58,21 @@ const writeFile = (gen, art, songName, mus) => {
         }
     }
 
-    if (fs.existsSync(path.join(__dirname, 'Genders', gen, art))) {
-        fs.writeFile(path.join(__dirname, 'Genders', gen, art, `${songName.replace(slash, '')}.json`),
+    if (fs.existsSync(artFolderPath)) {
+        fs.writeFile(songPath,
             JSON.stringify(music, null, 4),
             (err) => {
                 if (err) throw err
-                console.log('Song ' + songName + ' written!')
+                console.log('Song ' + songName + ' from artist ' + art + ' written!')
             })
     } else {
-        fs.mkdir(path.join(__dirname, 'Genders', gen, art), { recursive: true }, err => {
+        fs.mkdir(artFolderPath, { recursive: true }, err => {
             if (err) throw err
-
-            fs.writeFile(path.join(__dirname, 'Genders', gen, art, `${songName.replace(slash, '')}.json`),
+            fs.writeFile(songPath,
                 JSON.stringify(music, null, 4),
                 (err) => {
                     if (err) throw err
-                    console.log('Song ' + songName + ' written!')
+                    console.log('Song ' + songName + ' from artist ' + art + ' written!')
                 })
         })
     }
@@ -85,22 +88,26 @@ const removeAccents = (str) => {
 
 const main = async () => {
     for (let gen of gens){
+        console.log(`Gender> ${gen}`)
         let arts = await fetchArt(gen)
         for (let art of arts) {
+            console.log(`Artist> ${art}`)
             let artNorm = removeAccents(art).replace(replacer, '-').toLowerCase()
             let songs = await requestArtSongs(artNorm)
                                 .catch(err => console.log('Error requesting songs: ',err.message))
             try{
                 for(let song of songs){
-                    if (fs.existsSync(path.join(__dirname, 'Genders', gen, art, `${song.desc.replace(slash, '')}.json`))){
-                        console.log(`The music ${song.desc} already exists.`)
+                    let songPathJson = path.join(__dirname, 'Genders', gen, art, `${song.desc.replace(slash, '')}.json`)
+                    let songPathTxt  = path.join(__dirname, 'Genders', gen, art, `${song.desc.replace(slash, '')}.txt`)
+                    if (fs.existsSync(songPathJson) || fs.existsSync(songPathTxt)){
+                        console.log(`The music ${song.desc} from artist ${art} already exists.`)
                         continue
                     }
+                    console.log(`Song> ${song.desc}`)
                     await delay(3000).then(async () => {
                         let mus = await requestMusic(art, song.desc)
                                             .catch(err => console.log('Error requesting lyrics', err.message))
-                        if (mus !== null)
-                            writeFile(gen, art, song.desc, mus)
+                        writeFile(gen, art, song.desc, mus)
                     }).catch(err => console.log('Error on delay function: ', err.message))
                 }
             }catch(e){
@@ -109,4 +116,5 @@ const main = async () => {
         }
     }
 }
+
 main()
